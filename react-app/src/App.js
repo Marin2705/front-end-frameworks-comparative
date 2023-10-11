aaab5636b39ea5f8a7bc8927e29637b795fb93ed
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2023-10-11 16:08:48 +0200

    feat(react): add hello world component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const reactDocsUrl = "https://reactjs.org";
+
+const HelloWorld = ({ name }) => {
+  return <h1>Hello {name} !</h1>;
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HelloWorld name="World" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={reactDocsUrl}
           target="_blank"
           rel="noopener noreferrer"
         >
